e8a1e0496f42c1030a26464770dd4960a02192b6
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2022-08-19 15:23:32 +0300

    Add files via upload

---
+++ GG.go
@@ -0,0 +1,22 @@
+/*package main
+
+import "fmt"
+
+func main() {
+	var x float64
+	var R = fmt.Printf("%.2f\n", x)
+	fmt.Scan(&x)
+	if x == 0 || x < 0 {
+		fmt.Println("число", R, "не подходит")
+		/*fmt.Printf("число")
+		fmt.Printf("%.2f ", x)
+		fmt.Printf("не подходит")
+	} else if x > 10000 {
+		fmt.Println("%e", x)
+	} else {
+		x = x * x
+		fmt.Printf("%.4f\n", x) //4 знака после запятой
+	}
+
+}
+*/
